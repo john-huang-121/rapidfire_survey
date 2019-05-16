5cfe2174336d5166b45bbff5e8efb75faac7c313
john <johnhuang121@gmail.com>
2019-05-15 23:30:41 -0700

    changed database from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_14_184914) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rapidfire_answers", force: :cascade do |t|
     t.integer "attempt_id"
     t.integer "question_id"
